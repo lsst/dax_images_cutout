c51d6f997c732ad2595ecfe3814b710d4dc0157d
Tim Jenness <tjenness@lsst.org>
2026-06-16 17:11:51 -0700

    Try using a local butler for the cutout test

--- tests/test_imageCutoutsBackendV2.py
+++ tests/test_imageCutoutsBackendV2.py
@@ -27,31 +27,25 @@ import astropy.io.fits
 import astropy.units as u
 
 import lsst.images
-import lsst.resources
 import lsst.sphgeom
 import lsst.utils.tests
 from lsst.daf.butler import Butler
 from lsst.dax.images.cutout import CutoutMode, ImageCutoutFactory, projection_finders, stencils
 
+TESTDIR = os.path.abspath(os.path.dirname(__file__))
+
 
 class TestImageCutoutsBackendV2(lsst.utils.tests.TestCase):
     """Tests for ImageCutoutsBackend using lsst.images data models."""
 
-    @classmethod
-    def setUpClass(cls):
-        try:
-            cls.data_dir = lsst.utils.getPackageDir("testdata_image_cutouts")
-        except LookupError:
-            raise unittest.SkipTest("testdata_image_cutouts not setup.") from None
-
     def setUp(self):
         collection = "LSSTCam/runs/DRP/DP2/v30_0_8/DM-55060/deep_coadd_rewrite/20260612T200929Z"
-        self.butler = Butler.from_config(os.path.join(self.data_dir, "repo_dp2"), collections=collection)
+        self.butler = Butler.from_config(os.path.join(TESTDIR, "butler"), collections=collection)
         self.enterContext(self.butler)
 
-        # Image roughly at 19:42:31 -25:36:32
-        point = lsst.sphgeom.LonLat.fromDegrees(295.629167, -25.608889)
-        radius = lsst.sphgeom.Angle((10 * u.arcsec).to_value(u.rad))
+        # Try: RA = 0:01:01.7  Dec = -3:02:13
+        point = lsst.sphgeom.LonLat.fromDegrees(0.25708, -3.03694)
+        radius = lsst.sphgeom.Angle((3 * u.arcsec).to_value(u.rad))
         self.stencil = stencils.SkyCircle(point, radius)
 
         self.projectionFinders = (
@@ -59,7 +53,7 @@ class TestImageCutoutsBackendV2(lsst.utils.tests.TestCase):
             projection_finders.ReadComponentsAstropyFits(),
         )
 
-        self.dataId = {"patch": 76, "tract": 5428, "band": "r", "skymap": "lsst_cells_v2"}
+        self.dataId = {"patch": 76, "tract": 5428, "band": "g", "skymap": "lsst_cells_v2"}
 
     def test_extract_ref(self):
         """Test that extract_ref produces a reasonable cutout."""
@@ -87,17 +81,17 @@ class TestImageCutoutsBackendV2(lsst.utils.tests.TestCase):
                         raise RuntimeError(f"Unexpected cutout type: {type(result.cutout)}")
 
                 with self.subTest(cutout_mode=cutout_mode):
-                    self.assertEqual(box.x.size, 101)
-                    self.assertEqual(box.y.size, 101)
+                    self.assertEqual(box.x.size, 29)
+                    self.assertEqual(box.y.size, 28)
 
                     # We are reading these values from the fuzzed data that
                     # has no scientific content but we can test that each
                     # cutout is returning the same value. These numbers have
                     # not been validated by external tooling and will change
                     # on redoing the fuzzing.
-                    self.assertFloatsAlmostEqual(array[50, 49], 7.9808526039123535)
-                    self.assertFloatsAlmostEqual(array[1, 1], 2.6900875568389893)
-                    self.assertFloatsAlmostEqual(array[100, 100], 85.94110107421875)
+                    self.assertFloatsAlmostEqual(array[14, 15], 2.788926362991333)
+                    self.assertFloatsAlmostEqual(array[1, 1], 7.183607578277588)
+                    self.assertFloatsAlmostEqual(array[27, 28], -11.042882919311523)
 
                     output = cutoutBackend.process_ref(self.stencil, dataRef, cutout_mode=cutout_mode)
                     self.assertTrue(output.exists())
